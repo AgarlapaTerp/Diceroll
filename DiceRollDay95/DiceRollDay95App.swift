255582cc6f4ea8cff81862b632ad41d2b0354bcd
abgbro <codepath@abhigarlapati.com>
2024-05-12 14:03:29 -0400

    Initial Commit

---
+++ DiceRollDay95/DiceRollDay95App.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollDay95App.swift
+//  DiceRollDay95
+//
+//  Created by user256510 on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollDay95App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
